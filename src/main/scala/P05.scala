81c16e9528b48e5bcf72ea3a74d9ab2cb7c77c74
Paul Xie <pellinore.x@gmail.com>
2018-04-06 14:40:01 -0400

    question 5 & 6

---
+++ src/main/scala/P05.scala
@@ -0,0 +1,8 @@
+package main.scala
+
+object P05 {
+
+  def reverseOf[A](a: List[A]): List[A] = {
+    (List[A]() /: a) ((r, h) => h :: r) // appending r to h because it is easier to point r to a new head
+  }
+}
