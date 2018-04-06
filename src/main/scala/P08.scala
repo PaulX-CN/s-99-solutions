c55914d3cd0cddd4fc3c4b0d6a93c27c07d61e91
Paul Xie <pellinore.x@gmail.com>
2018-04-06 17:18:59 -0400

    question 7 & 8

---
+++ src/main/scala/P08.scala
@@ -0,0 +1,35 @@
+package main.scala
+
+object P08 {
+
+  def compress(a: List[Any]): List[Any] = {
+    def _compress(previous: Any, newList: List[Any], curList: List[Any]): List[Any] = curList match {
+      case head :: tail if head == previous => _compress(previous, newList, tail)
+      case head :: tail                     => _compress(head, head :: newList, tail)
+      case Nil                              => newList.reverse
+    }
+
+    _compress(None, Nil, a)
+  }
+
+  def compressWithDrop(a: List[Any]): List[Any] = a match {
+    case head :: tail => head :: compressWithDrop(tail.dropWhile(_ == head))
+    case Nil          => Nil
+  }
+
+  def compressWithDropTailRecursive(a: List[Any]): List[Any] = {
+    def _compress(newList: List[Any], curList: List[Any]): List[Any] = curList match {
+      case head :: tail => _compress(head :: newList, tail.dropWhile(_ == head))
+      case Nil          => newList.reverse
+    }
+
+    _compress(Nil, a)
+  }
+
+  def compressFunctional(a: List[Any]): List[Any] = {
+    (a :\ List[Any]()) { (income, previous) =>
+      if (previous.isEmpty || income != previous.head) income :: previous
+      else previous
+    }
+  }
+}
