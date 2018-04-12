c8eddb7c82c0eecffb0d90fb0856aed0cff48abf
Paul-X <pellinore.x@gmail.com>
2018-04-12 00:04:32 -0400

    problem 21

--- src/main/scala/P20.scala
+++ src/main/scala/P20.scala
@@ -1,12 +1,15 @@
 package main.scala
 
+import java.util.NoSuchElementException
+
 object P20 {
 
   def removeAt(n: Int, a: List[Any]): (List[Any], Any) = {
     def _take(curN: Int, newList: List[Any], curList: List[Any]): (List[Any], Any) = curList match {
+      case _ if n < 0          => throw new NoSuchElementException
       case h :: t if curN == n => (newList.reverse ::: t, h)
       case h :: t              => _take(curN + 1, h :: newList, t)
-      case Nil                 => (newList.reverse, None)
+      case Nil                 => throw new NoSuchElementException
     }
 
     _take(0, Nil, a)
---
+++ src/test/scala/testP21.scala
@@ -0,0 +1,11 @@
+import main.scala.P21._
+import org.scalatest._
+
+
+class testP21 extends FlatSpec with Matchers {
+
+  "insertAt" should "insert element at nth of list" in {
+    val mockList = List('a, 'b, 'c, 'd)
+    insertAt('new, 1, mockList) should be(List('a, 'new, 'b, 'c, 'd))
+  }
+}
