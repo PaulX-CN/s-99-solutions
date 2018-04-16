230376d2584f5fd0416e3256aa661044cd0503a9
Paul Xie <pellinore.x@gmail.com>
2018-04-16 17:19:00 -0400

    question 28,  29

---
+++ src/main/scala/P27.scala
@@ -0,0 +1,23 @@
+package main.scala
+
+object P27 {
+
+  import P23NP24NP25P26.combination
+
+  def group3[A](ls: List[A]): List[List[List[A]]] = {
+    for {
+      a <- combination(2, ls)
+      remainList = ls diff a
+      b <- combination(3, remainList)
+    } yield List(a, b, remainList diff b)
+  }
+
+  def groupN[A](n: List[Int], ls: List[A]): List[List[List[A]]] = n match {
+    case n :: ns => {
+      combination(n, ls) flatMap { firstCombination => // take all the possible first combination
+        groupN(ns, ls diff firstCombination) map { firstCombination :: _ } // put in front of subcombination
+      }
+    }
+    case _       => List(Nil)
+  }
+}
---
+++ src/test/scala/testP28NP29.scala
@@ -0,0 +1,23 @@
+import main.scala.P28NP29._
+import org.scalatest._
+
+class testP28NP29 extends FlatSpec with Matchers {
+
+  "lsort" should "sort a list by length of the item" in {
+    val mockList = List(List('a, 'b, 'c), List('d, 'e), List('f, 'g, 'h), List('d, 'e), List('i, 'j, 'k, 'l),
+      List('m, 'n), List('o))
+    lsortByQuickSort(mockList) should be(
+      List(List('o), List('d, 'e), List('d, 'e), List('m, 'n), List('a, 'b, 'c), List('f, 'g, 'h),
+        List('i, 'j, 'k, 'l)))
+  }
+
+
+  "sortByFreq" should "sort a list by the frequency of length of the sublists appeared in this list" in {
+    val mockList = List(List('a, 'b, 'c), List('d, 'e), List('f, 'g, 'h), List('d, 'e), List('i, 'j, 'k, 'l),
+      List('m, 'n), List('o))
+    sortByFreq(mockList) should be(
+      List(List('i, 'j, 'k, 'l), List('o), List('a, 'b, 'c), List('f, 'g, 'h), List('d, 'e), List('d, 'e),
+        List('m, 'n)))
+  }
+
+}
