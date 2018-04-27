158a92025cff7a08f776496f6f72488584a3cbc3
Paul-X <pellinore.x@gmail.com>
2018-04-26 20:21:52 -0400

    question 50 finished

--- src/test/scala/logic/testP50.scala
+++ src/test/scala/logic/testP50.scala
@@ -7,10 +7,10 @@ class testP50 extends FlatSpec with Matchers {
 
   "constructTree" should "return a tree based on given leaves" in {
     val mockLeaf = List(Leaf(None, 2))
-    constructTree(Leaf(None, 0), mockLeaf) should be(Leaf(None, 2))
+    constructTree(mockLeaf) should be(Leaf(None, 2))
 
     val mockLeaves = List(Leaf(None, 3), Leaf(None, 1), Leaf(None, 4))
-    constructTree(Leaf(None, 0), mockLeaves) should be(
+    constructTree(mockLeaves) should be(
       Branch(
         Branch(Leaf(None, 1),
           Leaf(None, 3), 4)
@@ -18,11 +18,16 @@ class testP50 extends FlatSpec with Matchers {
     )
   }
 
-  "decodeTree" should "return bala" in {
+  "decodeTree" should "return decoded sequence of a tree" in {
     val mockTree = Branch(
       Branch(Leaf(None, 1),
         Leaf(None, 3), 4)
       , Leaf(None, 4), 8)
-    decodeTree(mockTree, 0) should be(List("a", 1))
+    decodeTree(mockTree, "") should be(List(("a", "1")))
+  }
+
+  "huffman" should "return huffman code of given name->frequence pair list" in {
+    val mockList = List(("a", 45), ("b", 13), ("c", 12), ("d", 16), ("e", 9), ("f", 5))
+    huffman(mockList).toSet should be(Set(("a","0"), ("b","101"), ("c","100"), ("d","111"), ("e","1101"), ("f","1100")))
   }
 }
